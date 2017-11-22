4e12b40d9e7a6219df4491ddaf83e72f83e013d3
Wan Cheul Kim <wckim88@gmail.com>
2017-11-21 20:37:36 -0800

    Checkpoint 5: Username prompt modal pops up when page loaded

--- app/scripts/controllers/UsernameCtrl.js
+++ app/scripts/controllers/UsernameCtrl.js
@@ -1,9 +1,12 @@
 (function() {
-  function UsernameCtrl($uibModalInstance) {
-    
+  function UsernameCtrl($uibModalInstance, $cookies) {
+    this.ok = function(username) {
+      $cookies.put('blocChatCurrentUser', username);
+      $uibModalInstance.close(console.log("Welcome \"" + $cookies.get('blocChatCurrentUser') + "\"!"));
+    };
   }
 
   angular
     .module('blocChat')
-    .controller('UsernameCtrl', ['$uibModalInstance', UsernameCtrl]);
+    .controller('UsernameCtrl', ['$uibModalInstance', '$cookies', UsernameCtrl]);
 })();
