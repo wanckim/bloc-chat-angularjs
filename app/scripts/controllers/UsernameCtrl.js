a58f394e6c859b7e0da15fef4a710796e6d64d15
Wan Cheul Kim <wckim88@gmail.com>
2017-11-21 22:16:49 -0800

    Checkpoint 5: Empty username doesn't get accepted

--- app/scripts/controllers/UsernameCtrl.js
+++ app/scripts/controllers/UsernameCtrl.js
@@ -1,8 +1,12 @@
 (function() {
   function UsernameCtrl($uibModalInstance, $cookies) {
     this.ok = function(username) {
-      $cookies.put('blocChatCurrentUser', username);
-      $uibModalInstance.close(console.log("Welcome \"" + $cookies.get('blocChatCurrentUser') + "\"!"));
+      if (!username) {
+        alert("Please enter username!");
+      } else {
+        $cookies.put('blocChatCurrentUser', username);
+        $uibModalInstance.close(console.log("Welcome \"" + $cookies.get('blocChatCurrentUser') + "\"!"));
+      }
     };
   }
 
