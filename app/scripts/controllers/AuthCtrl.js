c87753af9a3c129cb3001d9eca49e131e47162df
Wan Cheul Kim <wckim88@gmail.com>
2018-09-07 18:42:40 -0700

    New landing page with new login and signup modals (#2)

--- app/scripts/controllers/AuthCtrl.js
+++ app/scripts/controllers/AuthCtrl.js
@@ -1,5 +1,5 @@
 (function() {
-  function AuthCtrl(Auth, $state) {
+  function AuthCtrl(Auth, $state, $uibModalInstance) {
     this.user = {
       email: '',
       password: ''
@@ -7,6 +7,7 @@
 
     this.login = function() {
       Auth.$signInWithEmailAndPassword(this.user.email, this.user.password).then(function(auth) {
+        $uibModalInstance.close(console.log("Welcome \"" + auth.email + "\"!"));
         $state.go('home');
       }, function(error) {
         this.error = error;
@@ -15,14 +16,19 @@
 
     this.register = function() {
       Auth.$createUserWithEmailAndPassword(this.user.email, this.user.password).then(function(user) {
+        $uibModalInstance.close(console.log("Thank you for joining Let's Slack \"" + user.email + "\"!"));
         $state.go('home');
       }, function(error) {
         this.error = error;
       });
     };
+
+    this.cancel = function() {
+      $uibModalInstance.close();
+    }
   }
 
   angular
     .module('blocChat')
-    .controller('AuthCtrl', ['Auth', '$state', AuthCtrl]);
+    .controller('AuthCtrl', ['Auth', '$state', '$uibModalInstance', AuthCtrl]);
 })();
